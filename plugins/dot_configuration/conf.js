3cbe01d6953927bd7a5278bfff631d52174af83e
Tasa <thierry.allardsaintalbint@gmail.com>
2016-06-29 01:01:40 +0200

    add dot_configuration

---
+++ plugins/dot_configuration/conf.js
@@ -0,0 +1,16 @@
+module.exports =  {
+			url : "http://httpbin.org/",
+			urlEnvironnementTest : "http://192.168.43.231:4000/api/v1/dashboard/",
+			urlProd : "http://httpbin.org/",
+			meteo : {
+				test : "heho"
+			},
+			path : {
+				url :"/screens/path/from/:from/to/:to"		
+			}
+};
+	
+	
+	
+	
+
